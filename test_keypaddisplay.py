bd78c941bcd3dbebb309504ecabc5035269db58d
Jonny Bergdahl <jonny@bergdahl.it>
2019-08-01 23:13:25 +0200

    Added test script

--- test_keypaddisplay.py
+++ test_keypaddisplay.py
@@ -1,26 +1,44 @@
-from m2m_keypaddisplay import KeypadDisplay, LED
+from time import sleep
+from m2m_keypad_display import KeypadDisplay, Leds
 
-def run_test(self):
+
+def run_tests():
     board = KeypadDisplay(0x3f)
-    print(board)
+
+    buttons = {
+        "A": board.button_a,
+        "B": board.button_b,
+        "Left": board.button_left,
+        "Up": board.button_up,
+        "Down": board.button_down,
+        "Right": board.button_right,
+        "C": board.button_c,
+    }
+    leds = {
+        "Red": Leds.RED,
+        "Yellow": Leds.YELLOW,
+        "Green": Leds.GREEN,
+        "All": Leds.ALL,
+        "None": Leds.NONE
+    }
+
+    for name in buttons:
+        button = buttons[name]
+        board.print(f"Press {name}")
+        while not button.is_pressed:
+            sleep(0.1)
 
     while True:
-        if board.button_a.is_pressed():
-            board.print("Button A")
-        if board.button_b.is_pressed():
-            board.print("Button B")
-        if board.button_left.is_pressed():
-            board.print("Button Left")
-        if board.button_up.is_pressed():
-            board.print("Button Up")
-        if board.button_down.is_pressed():
-            board.print("Button Down")
-        if board.button_right.is_pressed():
-            board.print("Button Right")
-        if board.button_c.is_pressed():
-            board.print("Button C")
+        for name in leds:
+            board.print(f"LED is {name}\nHold C to quit")
+            board.set_led(leds[name])
+            if board.button_c.is_pressed:
+                board.print("Quit...")
+                exit()
+            sleep(1)
+    
 
 
 
 if __name__ == "__main__":
-    run_test()
+    run_tests()
